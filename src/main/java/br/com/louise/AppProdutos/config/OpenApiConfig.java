a85b1bc59ef9555a61105016f4508e078e449127
louise <louisezlemos@gmail.com>
2025-11-24 10:41:34 -0300

    chore: configura swagger e flyway
    - Adiciona V1__Initial_Schema.sql com estrutura completa do banco
    - Ajusta nome da tabela tbl_order_items para compatibilidade JPA
    - Cria OpenApiConfig.java para habilitar autenticacao JWT no Swagger UI

--- src/main/java/br/com/louise/AppProdutos/config/OpenApiConfig.java
+++ src/main/java/br/com/louise/AppProdutos/config/OpenApiConfig.java
@@ -1,4 +1,33 @@
 package br.com.louise.AppProdutos.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "API AppProdutos",
+                version = "1.0",
+                description = "Documentação da API de E-commerce (Produtos, Pedidos, Auth, etc)",
+                contact = @Contact(name = "Suporte", email = "suporte@appprodutos.com")
+        ),
+        security = {
+                @SecurityRequirement(name = "bearerAuth")
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "Autenticação JWT via Bearer Token",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
 public class OpenApiConfig {
 }
